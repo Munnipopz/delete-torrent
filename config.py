1e8a96f8d47950897682f14b9efc8ad5d164d7fe
Munnipopz <66902087+Munnipopz@users.noreply.github.com>
2020-10-02 14:31:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-token = ''
+token = '1278080596:AAEaKKjj2vFwd-7kAfHYgYpeuGGBybfk5Ns'
 
-db = 'rutracker_base.sqlite'
+db = 'postgres://eyofrnogltxmwz:9f85e8584199a374615a67bfe18f73edc4afaf9451d16eed3aee5b179708589b@ec2-54-243-195-160.compute-1.amazonaws.com:5432/d325npis91qikn'
 
 categories_dict = 'categories_dict.json'
